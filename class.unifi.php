31084e2d35a5824d4c85a118db972ae3de346e22
danpospisil <danposp@gmail.com>
2016-11-28 02:33:36 +0100

    Code cleaned, list_clients method added

--- class.unifi.php
+++ class.unifi.php
@@ -2,25 +2,25 @@
 
 class unifiapi {
    
-   public $user="";
-   public $password="";
-   public $site="default";
-   public $baseurl="https://127.0.0.1:8443";
-   public $controller="3.2.8";
-   public $is_loggedin=false;
-   private $cookies="";
-   public $debug=false;
-   
-   function __construct($user="",$password="",$baseurl="",$site="",$controller="") {
+   public $user = "";
+   public $password = "";
+   public $site = "default";
+   public $baseurl = "https://127.0.0.1:8443";
+   public $controller = "5.2.9";
+   public $is_loggedin = false;
+   public $debug = false;
+   private $cookies = "";
+
+   function __construct($user = "", $password = "", $baseurl = "", $site = "", $controller = "") {
       if (!empty($user)) $this->user = $user;
       if (!empty($password)) $this->password = $password;
       if (!empty($baseurl)) $this->baseurl = $baseurl;
       if (!empty($site)) $this->site = $site;
       if (!empty($controller)) $this->controller = $controller;
-      if (strpos($controller,".")) {
-		$con_ver = explode(".",$controller);
-		$controller = $con_ver[0];
-	  }
+      if (strpos($controller, ".")) {
+         $con_ver = explode(".", $controller);
+         $controller = $con_ver[0];
+      }
       $this->controller = $controller;
    }
       
@@ -35,35 +35,33 @@ class unifiapi {
    */
    
    public function login() {
-      $this->cookies="";
-      $ch=$this->get_curl_obj();
+      $this->cookies = "";
+      $ch = $this->get_curl_obj();
       curl_setopt($ch, CURLOPT_HEADER, 1);
-      if($this->controller >= 4) {
-		//Controller 4
-		curl_setopt($ch, CURLOPT_REFERER, $this->baseurl."/login");
-		curl_setopt($ch, CURLOPT_URL, $this->baseurl."/api/login");
-		curl_setopt($ch, CURLOPT_POSTFIELDS,json_encode(array("username" => $this->user, "password" => $this->password)).":");
-	  } else {
-		//Controller 3
-		curl_setopt($ch, CURLOPT_URL, $this->baseurl."/login");
-	    curl_setopt($ch, CURLOPT_POSTFIELDS,"login=login&username=".$this->user."&password=".$this->password);
-	  }
-      $content=curl_exec($ch);
+      if ($this->controller >= 4) {
+         //Controller 4
+         curl_setopt($ch, CURLOPT_REFERER, $this->baseurl . "/login");
+         curl_setopt($ch, CURLOPT_URL, $this->baseurl . "/api/login");
+         curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode(array("username" => $this->user, "password" => $this->password)) . ":");
+      } else {
+         //Controller 3
+         curl_setopt($ch, CURLOPT_URL, $this->baseurl . "/login");
+         curl_setopt($ch, CURLOPT_POSTFIELDS, "login=login&username=" . $this->user . "&password=" . $this->password);
+      }
+      $content = curl_exec($ch);
       $header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
       $body = trim(substr($content, $header_size));
-      $code=curl_getinfo($ch,CURLINFO_HTTP_CODE);
-      curl_close ($ch);
+      $code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+      curl_close($ch);
       preg_match_all('|Set-Cookie: (.*);|U', substr($content, 0, $header_size), $results);
       if (isset($results[1])) {
-         $this->cookies = implode(';', $results[1]);
-         //if (!empty($body)) { // edit FredB => empty body is normal in 3.2.1
-            if (($code >= 200) && ($code < 400)) {
-               if (strpos($this->cookies,"unifises") !== FALSE) {
-                  $this->is_loggedin=true;
-                  }
-               }
-         //    }                // edit FredB
+      $this->cookies = implode(';', $results[1]);
+         if (($code >= 200) && ($code < 400)) {
+            if (strpos($this->cookies, "unifises") !== FALSE) {
+               $this->is_loggedin = true;
+            }
          }
+      }
       return $this->is_loggedin;
    }
    
@@ -72,10 +70,10 @@ class unifiapi {
    */
    public function logout() {
       if (!$this->is_loggedin) return false;
-      $return=false;
-      $content=$this->exec_curl($this->baseurl."/logout");
-      $this->is_loggedin=false;
-      $this->cookies="";
+      $return = false;
+      $content = $this->exec_curl($this->baseurl . "/logout");
+      $this->is_loggedin = false;
+      $this->cookies = "";
       return $return;   
       }
 
@@ -84,15 +82,15 @@ class unifiapi {
    paramater <mac address>,<minutes until expires from now>
    return true on success 
    */
-   public function authorize_guest($mac,$minutes) {
-      $mac=strtolower($mac);
+   public function authorize_guest($mac, $minutes) {
+      $mac = strtolower($mac);
       if (!$this->is_loggedin) return false;
-      $return=false;
-      $content=$this->exec_curl($this->baseurl."/api/s/".$this->site."/cmd/stamgr","json={'cmd':'authorize-guest', 'mac':'".$mac."', 'minutes':".$minutes."}");
-      $content_decoded=json_decode($content);
+      $return = false;
+      $content = $this->exec_curl($this->baseurl . "/api/s/" . $this->site . "/cmd/stamgr", "json={'cmd':'authorize-guest', 'mac':'" . $mac . "', 'minutes':" . $minutes."}");
+      $content_decoded = json_decode($content);
       if (isset($content_decoded->meta->rc)) {
          if ($content_decoded->meta->rc == "ok") {
-            $return=true;
+            $return = true;
          }      
       }
       return $return;   
@@ -104,14 +102,14 @@ class unifiapi {
    return true on success 
    */
    public function unauthorize_guest($mac) {
-      $mac=strtolower($mac);
+      $mac = strtolower($mac);
       if (!$this->is_loggedin) return false;
-      $return=false;
-      $content=$this->exec_curl($this->baseurl."/api/s/".$this->site."/cmd/stamgr","json={'cmd':'unauthorize-guest', 'mac':'".$mac."'}");
-      $content_decoded=json_decode($content);
+      $return = false;
+      $content = $this->exec_curl($this->baseurl . "/api/s/" . $this->site . "/cmd/stamgr", "json={'cmd':'unauthorize-guest', 'mac':'" . $mac."'}");
+      $content_decoded = json_decode($content);
       if (isset($content_decoded->meta->rc)) {
          if ($content_decoded->meta->rc == "ok") {
-            $return=true;
+            $return = true;
          }      
       }
       return $return;   
@@ -123,14 +121,14 @@ class unifiapi {
    return true on success 
    */
    public function reconnect_sta($mac) {
-      $mac=strtolower($mac);
+      $mac = strtolower($mac);
       if (!$this->is_loggedin) return false;
-      $return=false;
-      $content=$this->exec_curl($this->baseurl."/api/s/".$this->site."/cmd/stamgr","json={'cmd':'kick-sta', 'mac':'".$mac."'}");
-      $content_decoded=json_decode($content);
+      $return = false;
+      $content = $this->exec_curl($this->baseurl . "/api/s/" . $this->site . "/cmd/stamgr", "json={'cmd':'kick-sta', 'mac':'" . $mac . "'}");
+      $content_decoded = json_decode($content);
       if (isset($content_decoded->meta->rc)) {
          if ($content_decoded->meta->rc == "ok") {
-            $return=true;
+            $return = true;
          }      
       }
       return $return;   
@@ -142,14 +140,14 @@ class unifiapi {
    return true on success 
    */
    public function block_sta($mac) {
-      $mac=strtolower($mac);
+      $mac = strtolower($mac);
       if (!$this->is_loggedin) return false;
-      $return=false;
-      $content=$this->exec_curl($this->baseurl."/api/s/".$this->site."/cmd/stamgr","json={'cmd':'block-sta', 'mac':'".$mac."'}");
-      $content_decoded=json_decode($content);
+      $return = false;
+      $content = $this->exec_curl($this->baseurl . "/api/s/" . $this->site . "/cmd/stamgr", "json={'cmd':'block-sta', 'mac':'" . $mac . "'}");
+      $content_decoded = json_decode($content);
       if (isset($content_decoded->meta->rc)) {
          if ($content_decoded->meta->rc == "ok") {
-            $return=true;
+            $return = true;
          }      
       }
       return $return;   
@@ -161,14 +159,14 @@ class unifiapi {
    return true on success 
    */
    public function unblock_sta($mac) {
-      $mac=strtolower($mac);
+      $mac = strtolower($mac);
       if (!$this->is_loggedin) return false;
-      $return=false;
-      $content=$this->exec_curl($this->baseurl."/api/s/".$this->site."/cmd/stamgr","json={'cmd':'unblock-sta', 'mac':'".$mac."'}");
-      $content_decoded=json_decode($content);
+      $return = false;
+      $content = $this->exec_curl($this->baseurl . "/api/s/" . $this->site . "/cmd/stamgr", "json={'cmd':'unblock-sta', 'mac':'" . $mac . "'}");
+      $content_decoded = json_decode($content);
       if (isset($content_decoded->meta->rc)) {
          if ($content_decoded->meta->rc == "ok") {
-            $return=true;
+            $return = true;
          }      
       }
       return $return;   
@@ -180,14 +178,14 @@ class unifiapi {
    return true on success 
    */
    public function restart_ap($mac) {
-      $mac=strtolower($mac);
+      $mac = strtolower($mac);
       if (!$this->is_loggedin) return false;
-      $return=false;
-      $content=$this->exec_curl($this->baseurl."/api/s/".$this->site."/cmd/devmgr","json={'cmd':'restart', 'mac':'".$mac."'}");
-      $content_decoded=json_decode($content);
+      $return = false;
+      $content = $this->exec_curl($this->baseurl . "/api/s/" . $this->site . "/cmd/devmgr", "json={'cmd':'restart', 'mac':'" . $mac . "'}");
+      $content_decoded = json_decode($content);
       if (isset($content_decoded->meta->rc)) {
          if ($content_decoded->meta->rc == "ok") {
-            $return=true;
+            $return = true;
          }      
       }
       return $return;   
@@ -198,18 +196,17 @@ class unifiapi {
    returns a array of access point objects
    */
    public function list_aps() {
-      $return=array();
+      $return = array();
       if (!$this->is_loggedin) return $return;
-      $return=array();
-      $content=$this->exec_curl($this->baseurl."/api/s/".$this->site."/stat/device","json={}");
-      $content_decoded=json_decode($content);
+      $content = $this->exec_curl($this->baseurl . "/api/s/" . $this->site . "/stat/device", "json={}");
+      $content_decoded = json_decode($content);
       if (isset($content_decoded->meta->rc)) {
          if ($content_decoded->meta->rc == "ok") {
             if (is_array($content_decoded->data)) {
                foreach ($content_decoded->data as $guest) {
-                  $return[]=$guest;
-                  }
+                  $return[] = $guest;
                }
+            }
          }      
       }
       return $return;   
@@ -220,44 +217,63 @@ class unifiapi {
    returns a array of guest objects
    */
    public function list_guests() {
-      $return=array();
+      $return = array();
       if (!$this->is_loggedin) return $return;
-      $return=array();
-      $content=$this->exec_curl($this->baseurl."/api/s/".$this->site."/stat/guest","json={}");
-      $content_decoded=json_decode($content);
+      $content = $this->exec_curl($this->baseurl . "/api/s/" . $this->site . "/stat/guest", "json={}");
+      $content_decoded = json_decode($content);
       if (isset($content_decoded->meta->rc)) {
          if ($content_decoded->meta->rc == "ok") {
             if (is_array($content_decoded->data)) {
                foreach ($content_decoded->data as $guest) {
-                  $return[]=$guest;
-                  }
+                  $return[] = $guest;
                }
+            }
          }      
       }
       return $return;   
    }  
 
    /*
+   list clients
+   returns a array of clients objects
+   */
+   public function list_clients() {
+      if (!$this->is_loggedin) return false;
+      $return = array();
+      $content = $this->exec_curl($this->baseurl . "/api/s/" . $this->site . "/stat/sta");
+      $content_decoded = json_decode($content);
+      if (isset($content_decoded->meta->rc)) {
+         if ($content_decoded->meta->rc == "ok") {
+            if (is_array($content_decoded->data)) {
+               foreach ($content_decoded->data as $client) {
+                  $return[] = $client;
+               }
+            }
+         }
+      }
+      return $return;
+   }
+
+   /*
    list vouchers 
    returns a array of voucher objects
    */
-   public function get_vouchers($create_time="") {
-      $return=array();
+   public function get_vouchers($create_time = "") {
+      $return = array();
       if (!$this->is_loggedin) return $return;
-      $return=array();
-      $json="";
+      $json = "";
       if (trim($create_time) != "") {
-         $json.="'create_time':".$create_time."";
+         $json .= "'create_time':" . $create_time . "";
       }
-      $content=$this->exec_curl($this->baseurl."/api/s/".$this->site."/stat/voucher","json={".$json."}");
-      $content_decoded=json_decode($content);
+      $content = $this->exec_curl($this->baseurl . "/api/s/" . $this->site . "/stat/voucher", "json={" . $json . "}");
+      $content_decoded = json_decode($content);
       if (isset($content_decoded->meta->rc)) {
          if ($content_decoded->meta->rc == "ok") {
             if (is_array($content_decoded->data)) {
                foreach ($content_decoded->data as $voucher) {
-                  $return[]=$voucher;
-                  }
+                  $return[] = $voucher;
                }
+            }
          }      
       }
       return $return;   
@@ -268,52 +284,52 @@ class unifiapi {
    paramater <minutes>,<number_of_vouchers_to_create>,<note>,<up>,<down>,<mb>
    returns a array of vouchers codes (Note: without the "-" in the middle)
    */
-   public function create_voucher($minutes,$number_of_vouchers_to_create=1,$note="",$up=0,$down=0,$Mbytes=0) {
-      $return=array();
+   public function create_voucher($minutes, $number_of_vouchers_to_create = 1, $note = "", $up = 0, $down = 0, $Mbytes = 0) {
+      $return = array();
       if (!$this->is_loggedin) return $return;
-      $json="'cmd':'create-voucher','expire':".$minutes.",'n':".$number_of_vouchers_to_create."";
+      $json = "'cmd':'create-voucher','expire':" . $minutes . ",'n':" . $number_of_vouchers_to_create . "";
       if (trim($note) != "") {
-         $json.=",'note':'".$note."'";
+         $json .= ",'note':'" . $note . "'";
       }
       if ($up > 0) {
-         $json.=",'up':".$up."";
+         $json .= ",'up':" . $up ."";
       }
       if ($down > 0) {
-         $json.=", 'down':".$down."";
+         $json .= ", 'down':" . $down . "";
       }
       if ($Mbytes > 0) {
-         $json.=", 'bytes':".$Mbytes."";
+         $json .= ", 'bytes':" . $Mbytes . "";
       }
-      $content=$this->exec_curl($this->baseurl."/api/s/".$this->site."/cmd/hotspot","json={".$json."}");
-      $content_decoded=json_decode($content);
-         if ($content_decoded->meta->rc == "ok") {
-            if (is_array($content_decoded->data)) {
-               $obj=$content_decoded->data[0];
-               foreach ($this->get_vouchers($obj->create_time) as $voucher)  {
-                  $return[]=$voucher->code;
-                  }
+      $content = $this->exec_curl($this->baseurl . "/api/s/" . $this->site . "/cmd/hotspot", "json={" . $json . "}");
+      $content_decoded = json_decode($content);
+      if ($content_decoded->meta->rc == "ok") {
+         if (is_array($content_decoded->data)) {
+            $obj = $content_decoded->data[0];
+            foreach ($this->get_vouchers($obj->create_time) as $voucher)  {
+               $return[] = $voucher->code;
             }
          }
+      }
       return $return;   
    }  
 
    private function exec_curl($url,$data="") {
-      $ch=$this->get_curl_obj();
+      $ch = $this->get_curl_obj();
       curl_setopt($ch, CURLOPT_URL, $url);
       if (trim($data) != "") {
-         curl_setopt($ch, CURLOPT_POSTFIELDS,$data);
+         curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
       } else {
          curl_setopt($ch, CURLOPT_POST, FALSE);
       }
-      $content=curl_exec($ch);
+      $content = curl_exec($ch);
       if ($this->debug == true) {
          print "---------------------\n<br>\n";
-         print $url."\n<br>\n";
-         print $data."\n<br>\n";
+         print $url . "\n<br>\n";
+         print $data . "\n<br>\n";
          print "---------------------\n<br>\n";
-         print $content."\n<br>\n";
+         print $content . "\n<br>\n";
          print "---------------------\n<br>\n";
-         }
+      }
       curl_close ($ch);
       return $content;
    }   
@@ -323,19 +339,18 @@ class unifiapi {
       curl_setopt($ch, CURLOPT_POST, TRUE);
       curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE); 
       curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
-      if($this->controller >= 4) {
-		curl_setopt($ch, CURLOPT_SSLVERSION, 6);
-	  } else {
-		curl_setopt($ch, CURLOPT_SSLVERSION, 3);
-	  }
+      if ($this->controller >= 4) {
+         curl_setopt($ch, CURLOPT_SSLVERSION, 6);
+      } else {
+         curl_setopt($ch, CURLOPT_SSLVERSION, 3);
+      }
       curl_setopt($ch , CURLOPT_RETURNTRANSFER, true);
       if ($this->debug == true) {
          curl_setopt($ch, CURLOPT_VERBOSE, TRUE);    
       }
       if ($this->cookies != "") {
          curl_setopt($ch, CURLOPT_COOKIE,  $this->cookies);
-         }
+      }
       return $ch;
    }   
-
 }
